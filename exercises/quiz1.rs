1c118944aa5e9bd05311d48752647ad6d6bb181b
lkf7700 <kf.luoyia@h3c.com>
2020-08-14 21:41:17 +0800

    work

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,19 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn ..... {
+const APPLE_PRIZE : i32 = 2;
+const CONDITION_SIZE : i32 = 40;
+const OFF_PRICE : i32 = 1;
+fn calculate_apple_price(size: i32) -> i32 {
+    if size > CONDITION_SIZE {
+        size
+    } else {
+        size * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
